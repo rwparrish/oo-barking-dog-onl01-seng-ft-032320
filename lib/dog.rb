61a6c2af0b7d41e9ee5944374d209e7dfdec9d6b
Ryan Parrish <rino3717@gmail.com>
2020-03-30 16:56:46 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,16 @@
 class Dog
   
-  def name=(dogs_name)
-    
+  def name=(dog_name)
+    @this_dogs_name = dog_name
   end
   
+  def name
+    @this_dogs_name
+  end
+  
+  def bark
+    puts "Woof!"
+  end
   
   
   
